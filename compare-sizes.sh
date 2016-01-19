6773471a20193324ccb736327d5297828894b072
Jim Witschey <jim.witschey@gmail.com>
2016-01-19 13:16:34 -0500

    more changes

--- compare-sizes.sh
+++ compare-sizes.sh
@@ -7,15 +7,10 @@ result_dir="result`date +%Y-%m-%d_%H_%M_%S`"
 mkdir $result_dir
 echo results in $result_dir
 
-for compressor in '' 'LZ4Compressor' 'SnappyCompressor' 'DeflateCompressor' ; do
+# for compressor in '' 'LZ4Compressor' 'SnappyCompressor' 'DeflateCompressor' ; do
+for compressor in '' ; do
   for profile in *.yaml ; do
-    # kill cassandra ; from mshuler
-    while pgrep -f CassandraDaemon; do
-      pkill -f CassandraDaemon
-      echo "SIGTERM sent"
-      sleep 2
-    done
-    echo "no CassandraDaemon"
+    killall -9 java || true
 
     stamp="`date +%Y-%m-%d_%H_%M_%S`"
     compressor_id_string="${compressor:-no_compressor}"
@@ -33,7 +28,7 @@ for compressor in '' 'LZ4Compressor' 'SnappyCompressor' 'DeflateCompressor' ; do
 
       echo "CREATE KEYSPACE stresscql WITH
             replication = {'class': 'SimpleStrategy', 'replication_factor': 3};" | ccm node1 cqlsh
-      echo "CREATE TABLE blogposts (
+      echo "CREATE TABLE stresscql.blogposts (
               domain text,
               published_date timeuuid,
               url text,
@@ -48,13 +43,13 @@ for compressor in '' 'LZ4Compressor' 'SnappyCompressor' 'DeflateCompressor' ; do
            " | ccm node1 cqlsh
 
 
-     ccm stress user profile=./$profile ops\(insert=1\)
+      ccm stress user profile=./$profile ops\(insert=1\) n=1M
       ccm node1 nodetool flush
       ccm node1 nodetool cfstats keyspace1
       ccm node1 nodetool compact
       ccm node1 nodetool cfstats keyspace1
       ccm stop
-    ) | tee "$result_dir"/"results_${compressor_id_string}_${stamp}"
+    ) &>1 | tee "$result_dir"/"results_${compressor_id_string}_${profile_id_string}_${stamp}"
     ( ccm stop ) # stop in a subshell so it can fail
 
   done
