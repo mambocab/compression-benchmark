6b8354447ac97eeb739943d1c0f846da32c61b9c
Jim Witschey <jim.witschey@gmail.com>
2016-01-19 11:51:45 -0500

    do the thing

--- compare-sizes.sh
+++ compare-sizes.sh
@@ -15,18 +15,18 @@ result_dir="result`date +%Y-%m-%d_%H_%M_%S`"
 mkdir $result_dir
 echo results in $result_dir
 
-for compressor in '' 'LZ4Compressor' 'SnappyCompressor' 'DeflateCompressor' ; do
-  for dist in '-pop dist=UNIFORM\(1..1000000\)' '' ; do
-    stamp="`date +%Y-%m-%d_%H_%M_%S`"
-    compressor_id_string="${compressor:-no_compressor}"
+UNIFORM_DIST_OPTS='-pop dist=UNIFORM(1..1000000)'
 
-    if [ "$dist" = '' ] ; then
-      dist_id_string='no_dist'
-    elif [ "$dist" = '-pop dist=UNIFORM\(1..1000000\)' ] ; then
-      dist_id_string='uniform_dist'
-    else
-      exit 1
-    fi
+for dist in "$UNIFORM_DIST_OPTS" '' ; do
+  stamp="`date +%Y-%m-%d_%H_%M_%S`"
+  compressor_id_string="${compressor:-no_compressor}"
+  if [ "$dist" = '' ] ; then
+    dist_id_string='no_dist'
+  elif [ "$dist" = "$UNIFORM_DIST_OPTS" ] ; then
+    dist_id_string='uniform_dist'
+  else
+    exit 1
+  fi
 
     (
       ccm create "with-${compressor_id_string}_${dist_id_string}_${stamp}" -n 1 -v "${CASSANDRA_VERSION:-3.2}"
@@ -43,9 +43,9 @@ for compressor in '' 'LZ4Compressor' 'SnappyCompressor' 'DeflateCompressor' ; do
 
       ccm stress write n=10M $extra_opts
       ccm node1 nodetool flush
-      ccm node1 nodetool cfstats keyspace1 | grep 'SSTable Compression Ratio:'
+      ccm node1 nodetool cfstats keyspace1
       ccm node1 nodetool compact
-      ccm node1 nodetool cfstats keyspace1 | grep 'SSTable Compression Ratio:'
+      ccm node1 nodetool cfstats keyspace1
       ccm stop
     ) | tee "$result_dir"/"results_${compressor_id_string}_${stamp}"
     ( ccm stop ) # stop in a subshell so it can fail
